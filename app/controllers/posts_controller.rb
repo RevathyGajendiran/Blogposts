9a6671293b39b3f1f61858113fa8b7f41766abdf
RevathyGajendiran <revathy9677@gmail.com>
2018-09-25 02:43:53 +0900

    'section3'

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+def index
+@content_first = "This is some first";
+@content_second ="This is some second";
+end
+def new
+
+end
+def create
+
+end
+def edit
+
+end
+def update
+
+end
+def show
+
+end
+def destroy
+
+end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+ 
+
+  get 'home2/index'
+  root 'home2#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :posts
+  
 end
